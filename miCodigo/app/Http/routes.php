695a57ee5871cb2b51eba11dbd8a5dd2225194de
victor <viktorolaya@gmail.com>
2016-05-09 21:46:03 -0500

    agregando grupo de rutas para la gestion de areas,mentores,grupos

--- miCodigo/app/Http/routes.php
+++ miCodigo/app/Http/routes.php
@@ -23,8 +23,32 @@ Route::group(['prefix'=>'/formularios-y-datos-semillero/'],function(){
 		return view('formulario-inscripcion');
 	});
 
+Route::get('home',function() {
+	return view('home');
+});
+
 
 });
+
+//Grupo De Rutas Admin --> Victor
+Route::group(['middleware'=>['web'],'prefix' => 'admin'], function () {
+	
+	/*
+	* Lo que hace resource, es tomar las funciones de un controlador y trasformarlas en rutas
+	*/
+	Route::resource('user','ControladorUsuarios');
+	Route::get('users/{id}/destroy', ['uses' => 'ControladorUsuarios@destroy','as' => 'admin.user.destroy']);
+
+	Route::resource('mentor','ControladorMentores');
+	Route::get('mentores/{id}/destroy', ['uses' => 'ControladorMentores@destroy','as' => 'admin.mentor.destroy']);
+
+	Route::resource('area','ControladorAreas');
+	Route::get('areas/{id}/destroy', ['uses' => 'ControladorAreas@destroy','as' => 'admin.area.destroy']);
+
+	Route::resource('grupo','ControladorGrupos');
+	Route::get('grupos/{id}/destroy', ['uses' => 'ControladorGrupos@destroy','as' => 'admin.grupo.destroy']);
+
+    });
 
 
 /*
