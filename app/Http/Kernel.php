d8fe0e27a5fa543673343c5a9cc8750097cef796
sbsromero <sebas-romero94@hotmail.com>
2016-05-12 03:01:32 -0500

    anterior

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -55,6 +55,5 @@ class Kernel extends HttpKernel
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'admin' => App\Http\Middleware\VerifyAdmin,
     ];
 }
