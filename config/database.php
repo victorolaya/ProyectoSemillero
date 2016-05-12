33ab14fceef78c97533a10ae0ebf14f095ebe9fc
sbsromero <sebas-romero94@hotmail.com>
2016-05-12 02:53:22 -0500

    agregacion bases

--- config/database.php
+++ config/database.php
@@ -1,11 +1,10 @@
 <?php
-/*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $host = $url["host"];
 $username = $url["user"];
 $password = $url["pass"];
 $database = substr($url["path"], 1);
-*/
 
 return [
 
@@ -59,7 +58,7 @@ return [
             'prefix' => '',
         ],
 
-        'mysql' => [
+       /* 'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
@@ -72,9 +71,9 @@ return [
             'strict' => false,
             'engine' => null,
         ],
+*/
 
-
-        /*'mysql' => array(
+        'mysql' => array(
             'driver'    => 'mysql',
             'host'      => $host,
             'database'  => $database,
@@ -83,7 +82,7 @@ return [
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
-        ),*/
+        ),
 
         'pgsql' => [
             'driver' => 'pgsql',
